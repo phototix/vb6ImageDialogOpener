5c4568fa9c02c6187ad2e0fdc08947a3a719b8c8
Brandon Chong <phototix@hotmail.com>
2023-05-17 10:51:15 +0800

    Changes to VB.NET for supports on Win 10 and 11

---
+++ Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        Button1 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(1, -2)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(797, 405)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(275, 409)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(236, 29)
+        Button1.TabIndex = 1
+        Button1.Text = "Select Image"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "PictureDisplay"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+
+    Private Sub LoadImage()
+        Try
+            ' Create a new OpenFileDialog object.
+            Dim openFileDialog1 As New OpenFileDialog()
+
+            ' Set the filter options and filter index.
+            openFileDialog1.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG"
+            openFileDialog1.FilterIndex = 1
+
+            ' Display the OpenFileDialog.
+            If openFileDialog1.ShowDialog() = DialogResult.OK Then
+                ' Load the selected image into the PictureBox.
+                PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+                PictureBox1.Image = Image.FromFile(openFileDialog1.FileName)
+            End If
+
+            ' Set the anchor property to Top And Left
+            PictureBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Left
+
+            ' Set the location of the PictureBox to the top-left corner of the window.
+            PictureBox1.Location = New Point(0, 0)
+
+        Catch ex As Exception
+            ' Display an error message if an exception occurs.
+            MessageBox.Show("An error occurred while loading the image: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        LoadImage()
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        LoadImage()
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        Try
+            ' Resize the PictureBox to fit the form.
+            PictureBox1.Width = ClientSize.Width
+            PictureBox1.Height = ClientSize.Height
+
+            ' Center and anchor the "Select Image" button at the bottom of the form.
+            Button1.Left = (ClientSize.Width - Button1.Width) / 2
+            Button1.Top = ClientSize.Height - Button1.Height
+            Button1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+
+            ' Set the anchor property to Top And Left
+            PictureBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Left
+
+            ' Set the location of the PictureBox to the top-left corner of the window.
+            PictureBox1.Location = New Point(0, 0)
+
+        Catch ex As Exception
+            ' Display an error message to the user.
+            MessageBox.Show("An error occurred while resizing the form: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set the TopMost property of the form to True.
+        Me.TopMost = True
+
+        Try
+            ' Load the default image into the PictureBox.
+            PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+            PictureBox1.Image = Image.FromFile("default.jpg")
+
+            ' Set the default aspect ratio to 4:3.
+            Dim aspectRatio As Double = 4.0 / 3.0
+            Dim newHeight As Integer = CInt(PictureBox1.Width / aspectRatio)
+            PictureBox1.Height = newHeight
+
+        Catch ex As Exception
+            ' Display an error message to the user.
+            MessageBox.Show("Welcome! Please select an image to start!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+End Class
