e66e718174407111fbf071e09358088a1d0bd65d
Brandon Chong <phototix@hotmail.com>
2023-05-30 16:34:38 +0800

    Move resize & relocate image box in a function

--- Form1.vb
+++ Form1.vb
@@ -2,43 +2,7 @@
 
 Public Class Form1
 
-    Private Sub LoadImage()
-        Try
-            ' Create a new OpenFileDialog object.
-            Dim openFileDialog1 As New OpenFileDialog()
-
-            ' Set the filter options and filter index.
-            openFileDialog1.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG"
-            openFileDialog1.FilterIndex = 1
-
-            ' Display the OpenFileDialog.
-            If openFileDialog1.ShowDialog() = DialogResult.OK Then
-                ' Load the selected image into the PictureBox.
-                PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
-                PictureBox1.Image = Image.FromFile(openFileDialog1.FileName)
-            End If
-
-            ' Set the anchor property to Top And Left
-            PictureBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Left
-
-            ' Set the location of the PictureBox to the top-left corner of the window.
-            PictureBox1.Location = New Point(0, 0)
-
-        Catch ex As Exception
-            ' Display an error message if an exception occurs.
-            MessageBox.Show("An error occurred while loading the image: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        End Try
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        LoadImage()
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-        LoadImage()
-    End Sub
-
-    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+    Private Sub PositionImage()
         Try
             ' Resize the PictureBox to fit the form.
             PictureBox1.Width = ClientSize.Width
@@ -81,7 +45,48 @@ Public Class Form1
             ' Display an error message to the user.
             MessageBox.Show("An error occurred while resizing the form: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
+    End Sub
 
+    Private Sub LoadImage()
+        Try
+            ' Create a new OpenFileDialog object.
+            Dim openFileDialog1 As New OpenFileDialog()
+
+            ' Set the filter options and filter index.
+            openFileDialog1.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG"
+            openFileDialog1.FilterIndex = 1
+
+            ' Display the OpenFileDialog.
+            If openFileDialog1.ShowDialog() = DialogResult.OK Then
+                ' Load the selected image into the PictureBox.
+                PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+                PictureBox1.Image = Image.FromFile(openFileDialog1.FileName)
+            Else
+                PositionImage()
+            End If
+
+            ' Set the anchor property to Top And Left
+            PictureBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Left
+
+            ' Set the location of the PictureBox to the top-left corner of the window.
+            PictureBox1.Location = New Point(0, 0)
+
+        Catch ex As Exception
+            ' Display an error message if an exception occurs.
+            MessageBox.Show("An error occurred while loading the image: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        LoadImage()
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        LoadImage()
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        PositionImage()
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
